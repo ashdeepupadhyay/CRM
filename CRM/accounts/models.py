261949b5e93e3ad0ec748076cc665e8d5a9dceff
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-13 20:14:49 +0530

    added the foreign key ro Order

--- CRM/accounts/models.py
+++ CRM/accounts/models.py
@@ -27,8 +27,8 @@ class Order(models.Model):
 			('Out for delivery', 'Out for delivery'),
 			('Delivered', 'Delivered'),
 			)
-    #customer = 
-	#product =
+    customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 	 
