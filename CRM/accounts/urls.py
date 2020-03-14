f0ae372e90e0a61e92e84c078fe21c913ca88f01
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-14 10:19:50 +0530

    namespacing the URLS

--- CRM/accounts/urls.py
+++ CRM/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='accounts'
+
 urlpatterns = [
     path('',views.home,name='home' ),
     path('customer/<str:pk_test>',views.customer,name='customer'),
