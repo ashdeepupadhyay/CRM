a8c5faf0c5cde3de374a5591ec88701f04c25c25
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-14 14:14:04 +0530

    added the date filter

--- CRM/accounts/filters.py
+++ CRM/accounts/filters.py
@@ -3,6 +3,8 @@ from .models import *
 from django_filters import DateFilter
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name="date_created",lookup_expr='gte')
+    end_date = DateFilter(field_name="date_created",lookup_expr='lt')
     class Meta:
         model = Order
         fields = '__all__'
