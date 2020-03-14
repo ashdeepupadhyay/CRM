557b0aef9c7adc836ca0529c07f2cff1bfcbfdca
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-14 12:57:07 +0530

    using a inlineform set for having multiple forms(for doing multiple orders)

--- CRM/accounts/views.py
+++ CRM/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
+from django.forms import inlineformset_factory
 
 # Create your views here.
 def home(request):
@@ -39,19 +40,20 @@ def customer(request,pk_test):
     return render(request,'accounts/customer.html',context)
 
 def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'))
     customer=Customer.objects.get(id=pk)
-    form =OrderForm(initial={
-        'customer':customer
-    })
+    formset = OrderFormSet(instance=customer)
+    #form =OrderForm(initial={    'customer':customer })
     if request.method =="POST":
         #print('Printing Post',request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset=OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
     context={
-        'form':form
+        'formset':formset
     }
     return render(request,'accounts/order_form.html',context)
 
