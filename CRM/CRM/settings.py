3576d0fbcd37ff74f00f7f30c49488bba592b4dc
ashdeep <ashdeepupadhyay@gmail.com>
2020-03-20 20:46:18 +0530

    not able to get emails though password send page gets render

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -127,3 +127,13 @@ STATICFILES_DIRS=[
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
+#SMTP Configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '****************'
+EMAIL_HOST_PASSWORD = '******'
+SERVER_EMAIL = '********'
+
--- CRM/accounts/urls.py
+++ CRM/accounts/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path,reverse_lazy
 from . import views
+
+from django.contrib.auth import views as auth_views
+
+
 
 app_name='accounts'
 
@@ -20,5 +24,21 @@ urlpatterns = [
     path('create_order/<str:pk>',views.createOrder,name='create_order'),
     path('update_order/<str:pk>',views.updateOrder,name='update_order'),
     path('delete_order/<str:pk>',views.deleteOrder,name='delete_order'),
+    
+    #auth_views
+    #submit email form
+    
+    path('reset_password/',auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')),name="reset_password"),
+    #path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_done'),
+
+    #path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html", success_url=reverse_lazy('accounts:password_reset_complete')),name='password_reset_done'),
+
+
+    #submit sent success message
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),    
+    #Link to password Rest form in email
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    #password successfully changed message
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 
 ]
